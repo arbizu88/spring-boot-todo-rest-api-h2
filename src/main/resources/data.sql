88163eb01d3cfe6b12636cba3ef01e605e80a382
Carlos Hidalgo <arbizu88@gmail.com>
2023-05-14 01:47:38 -0600

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'caha', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'caha', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'caha', 'Learn Microservices', sysdate(), false);
